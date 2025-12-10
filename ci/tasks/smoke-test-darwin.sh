9dec6f0e432bac876043f66f61905fab0e8349b7
Marco Molteni <marco.molteni@pix4d.com>
2025-12-10 14:49:34 +0100

    ci: simplify code for macOS smoke tests

--- ci/tasks/smoke-test-darwin.sh
+++ ci/tasks/smoke-test-darwin.sh
@@ -1,33 +1,21 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
 
+banner() {
+    echo
+    echo "######################################################################"
+    echo "$*"
+}
+
 : "${VERSION?The pipeline must set this parameter}"
-
 cd artifacts
-# ITERATE OVER ALL THE DARWIN ARCHIVES
-for MAC_ARCHIVE in $(ls *.zip); do
-    # CREATE A SPECIFIC DIRECTORY FOR THE ARTIFACT TO PREVENT OVERWRITING
-    ARTIFACT_DIRECTORY=$(basename $MAC_ARCHIVE .zip)
-    mkdir $ARTIFACT_DIRECTORY
-    unzip $MAC_ARCHIVE -d $ARTIFACT_DIRECTORY
 
-    echo
-    echo Entering $ARTIFACT_DIRECTORY
-    cd $ARTIFACT_DIRECTORY
+unzip *.zip
 
-    echo
-    echo Smoke tests
-    echo
-    echo "Test if aws-vault has embedded the correct version"
-    # We need to redirect stderr to stdout because aws-vault's CLI library prints version to stderr.
-    ./aws-vault --version 2>&1 | grep "$VERSION"
-    echo
-    echo "Test if aws-vault prints help for a subcommand"
-    ./aws-vault exec --help
+banner "Test if aws-vault has embedded the correct version"
+# We need to redirect stderr to stdout because aws-vault's CLI library prints version to stderr.
+./aws-vault --version 2>&1 | grep "$VERSION"
 
-    echo
-    echo Exiting $ARTIFACT_DIRECTORY
-    cd ..
-
-done
+banner "Test if aws-vault prints help for a subcommand"
+./aws-vault exec --help
